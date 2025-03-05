234e58be82f3dc857e52caf806f3a806bb1918d8
SaberHub-dev <saberm@colonybyte.com>
2025-03-05 13:12:43 +0000

    User Authentification( admin , parent ,staff)

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,6 @@ export async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle(swaggerTitle)
     .setDescription(swaggerDescription)
-    //.setContact('{ jc - develop }',  'https://github.com/JulianCallejas', 'micorreo@example.com')    //contact options website and email
     .setVersion('1.0')
     .addBearerAuth()
     .build();
@@ -38,8 +37,8 @@ export async function bootstrap() {
   app.enableCors();
 
 
-  await app.listen(3000);
-  Logger.log(`App running on Port 3000`);
+  await app.listen(3001);
+  Logger.log(`App running on Port 3001`);
 
 
 }
